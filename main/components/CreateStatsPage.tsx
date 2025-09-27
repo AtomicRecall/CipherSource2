32231be4aea13728f9ecb0d12d9173bf3ef9dca1
atomic <danielyurskiyimportant@gmail.com>
2025-09-27 02:56:06 -0400

    Alpha 1.1000000001
    - Fixing a small footer issue lol

--- main/components/CreateStatsPage.tsx
+++ main/components/CreateStatsPage.tsx
@@ -1121,10 +1121,6 @@ function CreateStatsPage({
                   </div>
                 )}
               </div>
-                            <footer className="bottom-0 mt-auto flex flex-col items-center justify-center pointer-events-none h-0">
-                    <span className="text-white ">&copy; AtomicRecall 2025</span>
-                    <p className="text-background font-bold text-shadow-lg">CS2 Alpha 1.10</p>
-              </footer>
             </Card>
             
           ) : null}
