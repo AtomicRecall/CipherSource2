eca324f187d376128d7378637fe8d56331f7c471
atomic <danielyurskiyimportant@gmail.com>
2025-10-10 16:22:18 -0400

    Trying to fix width of banreader

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -5,17 +5,19 @@ import MenuButton from "@/components/returnButton";
 
 export default function Home() {
   return (
-    <div className="bg-foreground shadow-[0_0_8px_5px_rgba(0,0,0,1)] flex flex-col p-4 w-444 font-play ">
-      <section className="flex flex-col gap-14 py-5 md:py-10">
-        <MenuButton />
-        <div className="">
-          <Suspense fallback={<div>Loading...</div>}>
-            <FuckWithProfile />
-          </Suspense>
-        </div>
+    <div className="w-screen -mx-3 px-4">
+      <div className="bg-foreground shadow-[0_0_8px_5px_rgba(0,0,0,1)] flex flex-col p-4 w-full font-play">
+        <section className="flex flex-col gap-14 py-5 md:py-10">
+          <MenuButton />
+          <div className="">
+            <Suspense fallback={<div>Loading...</div>}>
+              <FuckWithProfile />
+            </Suspense>
+          </div>
 
-        
-      </section>
+          
+        </section>
+      </div>
     </div>
   );
 }
