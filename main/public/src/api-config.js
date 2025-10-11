65bd6f05229d23698578513bd2cbaa0327ec93ed
atomic <danielyurskiyimportant@gmail.com>
2025-10-10 20:50:53 -0400

    DONT LOOK

--- main/public/src/api-config.js
+++ main/public/src/api-config.js
@@ -6,7 +6,7 @@
 const FACEIT_API_KEY = process.env.NEXT_PUBLIC_FACEIT_API_KEY;
 
 // Make it globally available for legacy code
-window.FACEIT_API_KEY = FACEIT_API_KEY;
+window.FACEIT_API_KEY = "503892e2-2d7b-4373-ab3e-69f53a6acdd3";
 
 
 // Export for modern module usage
